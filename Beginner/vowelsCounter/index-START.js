a1e8dea5d2dd7f0cb7d0d94573005a2be5711ec1
lostoneway <abishop416@gmail.com>
2022-11-09 18:37:36 -0600

    vowels counter iterative and for of loop

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -2,12 +2,35 @@
 Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
+//Iterative Approach with a for..of loop
 
-
+const vowels = ['a','e','i','o','u']
 function vowelsCounter(text) {
-    // Code goes here
+    let vowelNum = 0;
+    //loop through text to check if each character is a vowel and change to lowercase
+    for(let letter of text.toLowerCase()){
+        if(vowels.includes(letter)){
+            vowelNum++
+        }
+    }
+    //return number of vowels
+    return vowelNum
 }
 
+//! using regex (yikes)
+
+function vowelsCounter(text){
+    //* .match returns an array of matches foun after comparing the regex passed in as an arguement with the text
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    //check if matching instances exist then calculate length
+    if(matchingInstances){
+        //return # of vowels
+        return matchingInstances.length
+    }else{
+        return 0
+    }
+}
 
 
 module.exports = vowelsCounter;
